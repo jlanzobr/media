b69fe7a84201ef1698d4989d060c571a49df4bfe
J. Andrew Lanz-O'Brien <jlanzobr@gmail.com>
2013-10-30 14:36:33 -0400

    Removed unused functionality from User model, created guest user for DatabaseSeeder.

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -11,7 +11,23 @@ class DatabaseSeeder extends Seeder {
 	{
 		Eloquent::unguard();
 
-		// $this->call('UserTableSeeder');
+		$this->call('UserTableSeeder');
+		
+		$this->command->info('User table seeded!');
+	}
+
+}
+
+class UserTableSeeder extends Seeder {
+
+	public function run()
+	{
+		DB::table('users')->delete();
+
+		User::create(array(
+			'username'  => 'guest',
+			'password'  => '$2a$08$l17kUazWvGoskTtW6szQlufpu6wku0xL0Bz74Ba/V4uxJLLZTB88G',
+		));
 	}
 
 }
--- app/models/User.php
+++ app/models/User.php
@@ -1,9 +1,8 @@
 <?php
 
 use Illuminate\Auth\UserInterface;
-use Illuminate\Auth\Reminders\RemindableInterface;
 
-class User extends Eloquent implements UserInterface, RemindableInterface {
+class User extends Eloquent implements UserInterface {
 
 	/**
 	 * The database table used by the model.
@@ -37,16 +36,6 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	public function getAuthPassword()
 	{
 		return $this->password;
-	}
-
-	/**
-	 * Get the e-mail address where password reminders are sent.
-	 *
-	 * @return string
-	 */
-	public function getReminderEmail()
-	{
-		return $this->email;
 	}
 
 }
