54281599b7c812b002a625c41ec07c7d7b5527e4
J. Andrew Lanz-O'Brien <jlanzobr@gmail.com>
2013-10-31 22:02:22 -0400

    Reworked views to use a header.

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
 
-<div>
-
+<div class="container">
+Stuff
 </div>
 @stop
--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -1,18 +1,11 @@
 <html>
 	<head>
+		<meta charset="UTF-8" />
 		<link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.0.0/css/bootstrap.min.css" type="text/css" />
+		<title>Media Indexing System</title>
 	</head>
 	<body>
-		<div class="container">
-			<h1>
-				<div class="pull-right">
-					{{ HTML::link(URL::to('/'), 'Home', array('class' => 'btn btn-primary')) }}
-					{{ HTML::link('films', 'Films Directory', array('class' => 'btn btn-primary')) }}
-					{{ HTML::link('television', 'Television Directory', array('class' => 'btn btn-primary')) }}
-				</div>
-			</h1>
-		</div>
-		<hr>
+		@include('header')
 		@yield('content')
 	</body>
 </html>
