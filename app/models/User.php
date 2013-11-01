636cb4ba4ac9a5f8f99072747e0720ead92e26ad
J. Andrew Lanz-O'Brien <jlanzobr@gmail.com>
2013-10-31 22:00:59 -0400

    Added password hasher-setter to user model.

--- app/models/User.php
+++ app/models/User.php
@@ -48,5 +48,10 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	{
 		return $this->email;
 	}
+	
+	public function setPasswordAttribute($value)
+	{
+		$this->attributes['password'] = Hash::make($value);
+	}
 
 }
