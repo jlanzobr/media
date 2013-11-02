2cdde150b60840e20879c9ded82208ad2ab29021
J. Andrew Lanz-O'Brien <jlanzobr@gmail.com>
2013-11-02 13:45:25 -0400

    Redirecting with input.

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -7,20 +7,28 @@ class UserController extends BaseController {
 	public function loginHandler()
 	{
 		$input = Input::all();
+		$data = array();
+		
+		if($old = Input::old('errors'))
+		{
+			$data['errors'] = $old;
+		}
 
 		$validator = Validator::make($input, array(
-			"username" => "required",
-			"password" => "required"
+			'username' => 'required',
+			'password' => 'required'
 		));
 
 		if($validator->passes())
 		{
 			return Redirect::to('/');
 		}
+		else
+		{
+			$data['errors'] = $validator->messages();
+		}
 		
-		$data["errors"] = $validator->messages();
-		
-		return View::make("login", $data);
+		return View::make('login', $data);
 	}
 	
 	public function showLoginPage()
