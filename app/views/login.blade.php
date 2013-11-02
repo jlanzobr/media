efb789128a37b91ef2de159d828e4d9271fd6dbf
J. Andrew Lanz-O'Brien <jlanzobr@gmail.com>
2013-11-02 15:58:24 -0400

    Login view no longer has links that cannot be accessed.

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -1,17 +1,22 @@
-@extends('layout')
+<html>
+	<head>
+		<meta charset='UTF-8' />
+		<link rel='stylesheet' href='http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.0.0/css/bootstrap.min.css' type='text/css' />
+		<title>Media Indexing System</title>
+	</head>
+	<body>
+		@if($error = $errors->first())
+			<div class='error'>
+				{{ $error }}
+			</div>
+		@endif
 
-@if($error = $errors->first())
-	<div class='error'>
-		{{ $error }}
-	</div>
-@endif
-
-@section('content')
-	{{ Form::open(array('route' => 'loginHandler', 'method' => 'post')) }}
-		{{ Form::label('username', 'Username') }}
-		{{ Form::text('username', Input::old('username')) }}
-		{{ Form::label('password', 'Password') }}
-		{{ Form::password('password') }}
-		{{ Form::submit('login') }}
-	{{ Form::close() }}
-@stop
+		{{ Form::open(array('route' => 'loginHandler', 'method' => 'post')) }}
+			{{ Form::label('username', 'Username') }}
+			{{ Form::text('username', Input::old('username')) }}
+			{{ Form::label('password', 'Password') }}
+			{{ Form::password('password') }}
+			{{ Form::submit('login') }}
+		{{ Form::close() }}
+	</body>
+</html>
