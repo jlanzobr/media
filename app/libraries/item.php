95d16e3559f17a0f52361c19a0c4f8b09b4747c2
J. Andrew Lanz-O'Brien <jlanzobr@gmail.com>
2013-11-02 19:47:35 -0400

    Wrote Item library for getting file/directory properties.

---
+++ app/libraries/item.php
@@ -0,0 +1,67 @@
+<?php 
+
+class Item
+{
+	/**
+	* Contents of the directory
+	* @var array
+	*/
+	protected $directory_contents = array();
+	
+	/**
+	* Contents of the directory
+	* Each item is an array containing name, year, and extension (if exists)
+	* @var array
+	*/
+	protected $directory_contents = array();
+
+	public function __construct($path = '')
+	{
+		$this->directory_contents = $this->getDirectoryContents($path);
+		$this->processItems();
+	}
+
+	function getDirectoryContents($path)
+	{
+		$raw_directory_contents = scandir($path);
+		$this->directory_contents = array_diff($raw_directory_contents, array('..', '.'));
+		
+		return $this;
+	}
+
+	public function processItems()
+	{
+		foreach($this->directory_contents as $name)
+		{
+			$items[] = getProperties($name);
+		}
+		
+		return $this;
+	}
+
+	function getProperties($name)
+	{
+		 # Split $name on either side of year
+		$year_pattern = '/ [(](\d{4})[)]/';
+		$split_name = preg_split($year_pattern, $name);
+		$split_name = array_filter($split_name);
+		
+		# Get name
+		$properties['name'] = trim($split_name[0]);
+		
+		# Get extension
+		if(count($split_name) > 1)
+		{
+			$extension = str_replace('.', '', $split_name[1]);
+			$properties['extension'] = trim($extension);
+		}
+		
+		# Get year
+		preg_match($year_pattern, $name, $matches);
+		$year = preg_replace("/[^0-9,.]/", "", $matches[0]);
+		$properties['year'] = trim($year);
+		
+		return $properties;
+	}
+	
+}
