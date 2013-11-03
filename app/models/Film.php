683111eaddfab6a7e6d002a0984af0ba7ff307ba
J. Andrew Lanz-O'Brien <jlanzobr@gmail.com>
2013-11-03 02:08:39 -0500

    Added Film and Television data models.

---
+++ app/models/Film.php
@@ -0,0 +1,14 @@
+<?php
+
+class Film extends Eloquent {
+
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'films';
+	
+	public $timestamps = true;
+
+}
---
+++ app/models/Television.php
@@ -0,0 +1,14 @@
+<?php
+
+class Television extends Eloquent {
+
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'television';
+	
+	public $timestamps = true;
+
+}
