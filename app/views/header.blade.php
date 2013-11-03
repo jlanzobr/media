835571f30f320988ea1047fc95b3dd7113d9b645
J. Andrew Lanz-O'Brien <jlanzobr@gmail.com>
2013-11-03 14:23:12 -0500

    Header now has title.

--- app/views/header.blade.php
+++ app/views/header.blade.php
@@ -1,10 +1,15 @@
 <div class='container'>
 	<h1>
-		<div class='pull-right'>
-			{{ HTML::link(URL::route('/'), 'Home', array('class' => 'btn btn-primary')) }}
-			{{ HTML::link(URL::to('films'), 'Films Directory', array('class' => 'btn btn-primary')) }}
-			{{ HTML::link(URL::to('television'), 'Television Directory', array('class' => 'btn btn-primary')) }}
-			{{ HTML::link(URL::route('logout'), 'Logout', array('class' => 'btn btn-primary')) }}
+		<div class='Row'>
+			<div class='pull-left'>
+				<h1>Media Indexing System</h1>
+			</div>
+			<div class='pull-right'>
+				{{ HTML::link(URL::route('/'), 'Home', array('class' => 'btn btn-primary')) }}
+				{{ HTML::link(URL::to('films'), 'Films Directory', array('class' => 'btn btn-primary')) }}
+				{{ HTML::link(URL::to('television'), 'Television Directory', array('class' => 'btn btn-primary')) }}
+				{{ HTML::link(URL::route('logout'), 'Logout', array('class' => 'btn btn-primary')) }}
+			</div>
 		</div>
 	</h1>
 </div>
