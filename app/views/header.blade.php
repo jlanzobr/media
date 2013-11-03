d9cb8d9e4b7d78ab28e971985c8601700a32ba04
J. Andrew Lanz-O'Brien <jlanzobr@gmail.com>
2013-11-03 16:47:21 -0500

    Added dynamic title for pages, set in controller.

--- app/views/header.blade.php
+++ app/views/header.blade.php
@@ -2,7 +2,11 @@
 	<h1>
 		<div class='Row'>
 			<div class='pull-left'>
-				<h1>Media Indexing System</h1>
+				@if( ! empty($title))
+					<h1>Media Indexing System: {{ $title }}</h1>
+				@else
+					<h1>Media Indexing System</h1>
+				@endif
 			</div>
 			<div class='pull-right'>
 				{{ HTML::link(URL::route('/'), 'Home', array('class' => 'btn btn-primary')) }}
--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -2,7 +2,11 @@
 	<head>
 		<meta charset='UTF-8' />
 		<link rel='stylesheet' href='http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.0.0/css/bootstrap.min.css' type='text/css' />
-		<title>Media Indexing System</title>
+		@if( ! empty($title))
+			<title>Media Indexing System: {{ $title }}</title>
+		@else
+			<title>Media Indexing System</title>
+		@endif
 	</head>
 	<body>
 		@include('header')
