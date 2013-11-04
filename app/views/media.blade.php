44908896462cce22478903c727643869ee644f6d
J. Andrew Lanz-O'Brien <jlanzobr@gmail.com>
2013-11-03 20:53:01 -0500

    Added sorting functionality to media view.

--- app/views/media.blade.php
+++ app/views/media.blade.php
@@ -1,57 +1,66 @@
 @extends('layout')
+
+{{ HTML::script('js/common.js') }}
+{{ HTML::script('js/css.js') }}
+{{ HTML::script('js/standardista-table-sorting.js') }}
+
 @include('header')
 
 @section('content')
 
 <div class='container'>
 	@if( ! empty($media))
-		<table class='table table-bordered table-hover'>
-			<tr class='success'>
-				<th>
-					Name
-				</th>
-				<th>
-					Year
-				</th>
-				@if( ! empty($media[0]->extension))
-				<th>
-					Extension
-				</th>
-				@endif
-				<th>
-					Rating
-				</th>
-				<th>
-					Description
-				</th>
-				<th>
-					Edit
-				</th>
-			</tr>
-			@foreach($media as $item)
-			<tr>
-				<td>
-					{{ $item->name }}
-				</td>
-				<td>
-					{{ $item->year }}
-				</td>
-				@if( ! empty($media[0]->extension))
-				<td>
-					{{ $item->extension }}
-				</td>
-				@endif
-				<td>
-					{{ $item->rating }}
-				</td>
-				<td>
-					{{ $item->description }}
-				</td>
-				<td>
-					<a class='btn btn-info' title='Edit' href="{{ URL::to('films' . $item->id) }}">Edit</a>
-				</td>
-			</tr>
-			@endforeach
+		<table class='table table-bordered table-hover sortable' id='sortable'>
+			<thead>
+				<tr class='success'>
+					<th>
+						Name
+					</th>
+					<th>
+						Year
+					</th>
+					@if( ! empty($media[0]->extension))
+					<th>
+						Extension
+					</th>
+					@endif
+					<th>
+						Rating
+					</th>
+					<th>
+						Description
+					</th>
+					<th>
+						Edit
+					</th>
+				</tr>
+			</thead>
+			<tbody>
+				@foreach($media as $item)
+					<tr>
+						<td>
+							{{ $item->name }}
+						</td>
+						<td>
+							{{ $item->year }}
+						</td>
+						@if( ! empty($media[0]->extension))
+						<td>
+							{{ $item->extension }}
+						</td>
+						@endif
+						<td>
+							{{ $item->rating }}
+						</td>
+						<td>
+							{{ $item->description }}
+						</td>
+						<td>
+							<a class='btn btn-info' title='Edit' href="{{ URL::to('films' . $item->id) }}">Edit</a>
+						</td>
+					</tr>
+				@endforeach
+			</tbody>
 		</table>
 	@else
 		No results.
