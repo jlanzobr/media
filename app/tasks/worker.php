099ac5d418fc3a1fe388c639399e0e03bd4236d7
J. Andrew Lanz-O'Brien <jlanzobr@gmail.com>
2013-11-04 19:17:19 -0500

    Update worker.php
    Uncommented films worker initialization.

--- app/tasks/worker.php
+++ app/tasks/worker.php
@@ -8,5 +8,5 @@ require_once 'app/libraries/worker.php';
 
 set_time_limit(0);
 
-//$films_worker = new Worker('/home/user/Films', 'Film');
+$films_worker = new Worker('/home/user/Films', 'Film');
 $television = new Worker('/home/user/Television', 'Television');
